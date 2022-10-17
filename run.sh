dfde489a8848e76d5af5740faeeb272ec941a8fa
Atai <zver.cm@gmail.com>
2022-10-17 13:55:49 +0600

    askdjaskjdas

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 # Reason for "-nolisten tcp", not documented within Xvfb manpages (for who knows what reason)
 # https://superuser.com/questions/855019/make-xvfb-listen-only-on-local-ip
-Xvfb :99 -screen 0 1024x768x24 -nolisten tcp &
-python3 test.py
+#Xvfb :99 -screen 0 1024x768x24 -nolisten tcp &
+#python3 test.py
+
+xvfb-run --server-args='-screen 0, 1024x768x24' --auto-servernum --server-num=1 python3 test.py
+
