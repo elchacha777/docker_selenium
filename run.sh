6f6ebff583066e905d7007fd4091f9dd19c68872
Atai <zver.cm@gmail.com>
2022-10-17 15:00:35 +0600

    asdkjaskjdasd

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 # https://superuser.com/questions/855019/make-xvfb-listen-only-on-local-ip
 #Xvfb :99 -screen 0 1024x768x24 -nolisten tcp &
 #python3 test.py
-
-xvfb-run --server-args='-screen 0, 1024x768x24' --auto-servernum --server-num=1 python3 test.py
+xvfb-run --server-args='-screen 0, 840x480x24' --auto-servernum --server-num=1 python3 test.py
 
